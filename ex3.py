6dd38861582a0d1a952fe8a1a18972d58f0ff8ae
Adrian Arumugam <apa@moogzy.net>
2014-07-05 18:02:06 +0800

    Exercise 3 - Testing print statements

---
+++ ex3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# Prints statement for counting chickens
+print "I will now count my chickens:"
+
+# Addition then divisions for hens
+print "Hens", 25.0 + 30.0 / 6.0
+
+# Subtractions then multiplication then modulus(remainder)
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Prints statement for counting eggs
+print "Now I will count the eggs:"
+
+# Addition, substraction, modulus(remainder), subtraction, divisions, addition
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Prints statements then provides true\false comparison between arithmetic statements
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# Prints value of each arithmetic statement.
+print "What is 3 + 2?", 3.0 + 2.0
+print "what is 5 - 7?", 5.0 -7.0
+
+# Prints statement
+print "Oh, that's why it's False."
+
+# Prints statement
+print "How about some more."
+
+# Provides true\false comparison between arithmetic statements
+print "Is it greater?", 5.0  > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
