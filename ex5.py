398cda9b9aba82a358897da5a7de4b55a2291afe
Adrian Arumugam <apa@moogzy.net>
2014-07-07 01:09:04 +0800

    Exercise 5 - format strings - Learn Python the hard way

---
+++ ex5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# Setting variables
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+# Print statements with the use of format strings
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+
+# Test %r formatter - print anything but pass it through repr() first
+# to get a string version of any object.
+print "What does formatter r  do? - %r." % name
