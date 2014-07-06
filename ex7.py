1070f71bbeceabfbd6b12a6087db31709b06c494
Adrian Arumugam <apa@moogzy.net>
2014-07-07 01:55:09 +0800

    Exercise 7 - More printing

---
+++ ex7.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# Printing statements using format strings.
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # Print 10 periods.
+
+# Setting variables for print statement below.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Print 'Cheese Burger' using variables above
+# the comma on the first print statement tells python
+# to continute printing on the same line even though
+# the print statement is broken up.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
