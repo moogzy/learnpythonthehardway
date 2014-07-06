144f248ac83870bae177c3a11d99ca704b10d16c
Adrian Arumugam <apa@moogzy.net>
2014-07-07 02:07:13 +0800

    Exercise 8 - Printing, Printing

---
+++ ex8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# String variable that contains formatters
+formatter = "%r %r %r %r"
+
+# print formatter string and feed variables to cover the formatters
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
