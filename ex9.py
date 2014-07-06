586a4d303a2adceed673b102f9e2c9db7c35c80d
Adrian Arumugam <apa@moogzy.net>
2014-07-07 02:15:38 +0800

    Exercise 9 - Printing, Printing, Printing

---
+++ ex9.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+""" Exercise 9 - Printing, Printing, Printing
+
+Author: Adrian Arumugam
+
+"""
+
+# Set variables
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print statements with given variable
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# User three double quotes to get multiple line output mode
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
